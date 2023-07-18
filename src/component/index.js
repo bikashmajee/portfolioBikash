d124dcc4b9819e140323acb32b10d6dd30ef8401
Bikash Majee <majee.bikash1998@gmail.com>
2023-07-18 20:15:42 +0530

    sec commit

--- src/component/index.js
+++ src/component/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Navbar from '../common/navbar'
 import Footer from '../common/footer'
 import './index.css'
-// import Logo from '../images/logo.png'
-// import Logo from '../images/body-logo.png'
 import Logo from '../images/bpreview.jpg'
 
 
@@ -12,7 +10,7 @@ function index() {
   return (
     <>
     <Navbar/>
-    <div className='row'>
+    {/* <div className='row'>
       <div className='column right'>
         <div className='heading'>
           <h2><samp>I'm</samp> Bikash</h2>
@@ -24,6 +22,20 @@ function index() {
         <div className='leftbody'>
           <img src={Logo}/>
         </div> 
+      </div>
+    </div> */}
+    <div class="container-fluid text-center">
+      <div class="row border">
+        <div class="col border">
+          <div className='heading'>
+          <h2><samp>I'm</samp> Bikash</h2>
+          <p>Software Enginner..</p>
+        </div>
+        </div>
+        <div class="col border">
+          <img src={Logo} class="rounded-circle" alt="..."/>
+
+        </div>
       </div>
     </div>
     {/* <Footer/> */}
