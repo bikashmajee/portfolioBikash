0fb635c9375742bfc6da2df7024a8d999371da43
Bikash Majee <majee.bikash1998@gmail.com>
2023-07-19 22:27:02 +0530

    forth commit

--- src/component/index.js
+++ src/component/index.js
@@ -25,18 +25,38 @@ function index() {
       </div>
     </div> */}
     <div class="container-fluid text-center">
-      <div class="row ">
-        <div class="col  p-3 ">
+      <div class="row p-5 ">
+        <div class="col d-flex align-items-center justify-content-center">
           <div className='heading'>
           <h2><samp>I'm</samp> Bikash</h2>
           <p>Software Enginner..</p>
         </div>
         </div>
-        <div class="col  p-3 ">
+        <div class="col d-flex align-items-center justify-content-center">
           <img src={Logo} class="rounded-circle " alt="..."/>
 
         </div>
       </div>
+    </div>
+
+
+    <div class="container-fluid text-center">
+      <div className='row border-top '>
+        <p className='p-4 fs-3'>Results-driven Software Engineer with expertise in React.js, JavaScript, and Spring Boot. Sharp analytical acumen, adept at conquering intricate challenges. Seeking a pivotal role to drive transformative growth and deliver impeccable software solutions.</p>
+        
+      </div>
+      {/* <div class="row ">
+        <div class="col d-flex align-items-center justify-content-center">
+          <div className='heading'>
+          <h2><samp>I'm</samp> Bikash</h2>
+          <p>Software Enginner..</p>
+        </div>
+        </div>
+        <div class="col d-flex align-items-center justify-content-center">
+          <img src={Logo} class="rounded-circle " alt="..."/>
+
+        </div>
+      </div> */}
     </div>
     {/* <Footer/> */}
 
